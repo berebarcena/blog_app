8ab9a9a09566967beb599609a6893e766c027501
Berenice <berebarcenarm@gmail.com>
2018-10-03 17:34:18 +0200

    added each blog post routes

--- app.js
+++ app.js
@@ -3,65 +3,101 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const Sequelize = require('sequelize');
-
-app.use(bodyParser.urlencoded({ extended: false }));
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 app.set('view engine', 'ejs');
 
-//get the css files
-app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 //db config
 const sequelize = new Sequelize({
   database: process.env.BLOGAPP,
   username: process.env.POSTGRES_USER,
   host: 'localhost',
   dialect: 'postgres',
-  define: {
-    timestamps: false, // true by default
-  },
+  storage: './session.postgres',
 });
 
 app.use(
   session({
-    secret: 'navi es bebe',
+    store: new SequelizeStore({
+      db: sequelize,
+      checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+      expiration: 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
+    }),
+    secret: 'dunno',
     saveUninitialized: true,
     resave: false,
   })
 );
 
-//defining the Models
-const User = sequelize.define('users', {
-  name: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
+//get the css files
+app.use(express.static('public'));
+
+//defining the User model
+const User = sequelize.define(
+  'users',
+  {
+    name: {
+      type: Sequelize.STRING,
+      unique: true,
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      unique: true,
+    },
   },
-  email: {
+
+  {
+    timestamps: false,
+  }
+);
+
+const Post = sequelize.define('posts', {
+  title: {
     type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
   },
-  password: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
+  body: {
+    type: Sequelize.TEXT,
   },
+  createdAt: Sequelize.DATEONLY,
+  updatedAt: Sequelize.DATEONLY,
 });
 
+const Comment = sequelize.define('comments', {
+  comment: {
+    type: Sequelize.TEXT,
+  },
+  createdAt: Sequelize.DATEONLY,
+  updatedAt: Sequelize.DATEONLY,
+});
+//defining the relationships
+User.hasMany(Post);
+Post.belongsTo(User);
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+//login is the home
 app.get('/', (req, res) => {
   res.render('login', { message: req.query.message });
 });
+
+//signup route
 app.get('/signup', (req, res) => {
   const error = req.query.error;
   const errorMsg =
     error === 'no-empty-fields' ? 'Sorry! but all the fields are required' : '';
   res.render('signup', { error: errorMsg });
 });
+
 //post request login
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  //email and password is needed
   if (!email) {
     res.redirect(
       '/?message=' + encodeURIComponent('Please fill out your email address.')
@@ -72,6 +108,7 @@ app.post('/login', (req, res) => {
       '/?message=' + encodeURIComponent('Please fill out your password.')
     );
   }
+  //find the user with the email from the req
   User.findOne({
     where: {
       email: email,
@@ -87,7 +124,7 @@ app.post('/login', (req, res) => {
         );
       }
     })
-    .catch(function(error) {
+    .catch(error => {
       console.error(error);
     });
 });
@@ -113,7 +150,74 @@ app.post('/signup', (req, res) => {
 });
 //render the profile
 app.get('/profile', (req, res) => {
-  res.render('profile', { userInfo: req.session.user });
+  const user = req.session.user;
+  return Post.findAll({
+    include: [
+      {
+        model: User,
+        where: { name: user.name },
+      },
+    ],
+  }).then(posts => {
+    res.render('profile', { userInfo: user, posts });
+  });
+});
+
+app.get('/new-post', (req, res) => {
+  res.render('new_post');
+});
+
+app.post('/new-post', (req, res) => {
+  const user = req.session.user.name;
+  const title = req.body.title;
+  const postBody = req.body.newPostBody;
+
+  User.findOne({
+    where: {
+      name: user,
+    },
+  })
+    .then(user => {
+      return user.createPost({
+        title: title,
+        body: postBody,
+      });
+    })
+    .then(post => {
+      //console.log(post);
+      res.redirect(`/all-posts/${post.id}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/all-posts', (req, res) => {
+  Post.findAll({
+    include: [
+      {
+        model: User,
+      },
+    ],
+  }).then(post => {
+    res.render('all_posts', { postData: post });
+  });
+});
+
+app.get('/all-posts/:postId', (req, res) => {
+  const postId = req.params.postId;
+  Post.findOne({
+    where: {
+      id: postId,
+    },
+    include: [
+      {
+        model: User,
+      },
+    ],
+  }).then(post => {
+    res.render('blog_post', { post });
+  });
 });
 
 //logout route
